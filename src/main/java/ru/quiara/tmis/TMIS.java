3befe941b301f0966333dcbb34abe48f229a3193
Quintair <Raitins050@gmail.com>
2023-08-13 16:22:06 +1000

    Initial Commit

---
+++ src/main/java/ru/quiara/tmis/TMIS.java
@@ -0,0 +1,13 @@
+package ru.quiara.tmis;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TMIS implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/ru/quiara/tmis/TMISClient.java
@@ -0,0 +1,13 @@
+package ru.quiara.tmis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TMISClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
