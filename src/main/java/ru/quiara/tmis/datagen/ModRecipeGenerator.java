872a6040ac2573ce5131be4bf17f395430dafd35
Quintair <Raitins050@gmail.com>
2023-08-23 01:14:38 +1000

    Create Recipe

--- src/main/java/ru/quiara/tmis/datagen/ModRecipeGenerator.java
+++ src/main/java/ru/quiara/tmis/datagen/ModRecipeGenerator.java
@@ -3,7 +3,13 @@ package ru.quiara.tmis.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import ru.quiara.tmis.item.ModItems;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ModRecipeGenerator extends FabricRecipeProvider {
@@ -16,6 +22,56 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, List.of(ModItems.ANCIENT_SHARD), RecipeCategory.MISC,
+                ModItems.ANCIENT_INGOT, 5f, 200, "ancient_shard");
+        offerBlasting(exporter, List.of(ModItems.ANCIENT_SHARD), RecipeCategory.MISC,
+                ModItems.ANCIENT_INGOT, 5f, 100, "ancient_shard");
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ANCIENT_SWORD)
+                .pattern("I")
+                .pattern("I")
+                .pattern("S")
+                .input('I', ModItems.ANCIENT_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.ANCIENT_INGOT), conditionsFromItem(ModItems.ANCIENT_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ANCIENT_SWORD)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.ANCIENT_PICKAXE)
+                .pattern("III")
+                .pattern(" S ")
+                .pattern(" S ")
+                .input('I', ModItems.ANCIENT_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.ANCIENT_INGOT), conditionsFromItem(ModItems.ANCIENT_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ANCIENT_PICKAXE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.ANCIENT_AXE)
+                .pattern("II")
+                .pattern("IS")
+                .pattern(" S")
+                .input('I', ModItems.ANCIENT_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.ANCIENT_INGOT), conditionsFromItem(ModItems.ANCIENT_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ANCIENT_AXE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ANCIENT_SHOVEL)
+                .pattern("I")
+                .pattern("S")
+                .pattern("S")
+                .input('I', ModItems.ANCIENT_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.ANCIENT_INGOT), conditionsFromItem(ModItems.ANCIENT_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ANCIENT_SHOVEL)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.ANCIENT_HOE)
+                .pattern("II")
+                .pattern(" S")
+                .pattern(" S")
+                .input('I', ModItems.ANCIENT_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.ANCIENT_INGOT), conditionsFromItem(ModItems.ANCIENT_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.ANCIENT_HOE)));
+
 
     }
 }
