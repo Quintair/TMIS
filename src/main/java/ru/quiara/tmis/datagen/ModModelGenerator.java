889bbcb87d1f8f170652a5e2822d6fd086eba2b9
Quintair <Raitins050@gmail.com>
2023-08-23 01:15:00 +1000

    Create Models

--- src/main/java/ru/quiara/tmis/datagen/ModModelGenerator.java
+++ src/main/java/ru/quiara/tmis/datagen/ModModelGenerator.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import ru.quiara.tmis.item.ModItems;
 
 public class ModModelGenerator extends FabricModelProvider {
 
@@ -20,6 +22,15 @@ public class ModModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.ANCIENT_SHARD, Models.GENERATED);
+        itemModelGenerator.register(ModItems.ANCIENT_INGOT, Models.GENERATED);
 
+        itemModelGenerator.register(ModItems.ANCIENT_SOUP, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.ANCIENT_SWORD, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.ANCIENT_PICKAXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.ANCIENT_AXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.ANCIENT_SHOVEL, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.ANCIENT_HOE, Models.HANDHELD);
     }
 }
